bc5aedb948d1e2385e3a5dff7ba88ba129903775
Mitra Roshanfekr <mitraroshanfekr05@gmail.com>
2025-09-11 16:52:54 +0330

    responsive classes added

--- src/components/products/productCard.tsx
+++ src/components/products/productCard.tsx
@@ -1,8 +1,12 @@
-
-const productCard = () => {
+const ProductCard = () => {
   return (
-    <div>productCard</div>
-  )
-}
+    <div className=" p-[2rem] bg-[#F5F5F7] flex flex-col gap-[1rem] rounded-[1rem] w-40rem md:w-[20rem]">
+      <p>dsdfsdfdgfd</p>
+      <p>dsdfsdfdgfd</p>
+      <p>dsdfsdfdgfd</p>
+      <p>dsdfsdfdgfd</p>
+    </div>
+  );
+};
 
-export default productCard
+export default ProductCard;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,9 @@
 const HomePage = () => {
   return (
-    <main className="p-[2rem]">
-      <div className="p-[2rem] bg-[#F5F5F7] flex flex-col gap-[1rem] rounded-[1rem]">
+    <main className="p-[2rem] flex">
+      <div
+        className="productCard p-[2rem] bg-[#F5F5F7] flex flex-col gap-[1rem] rounded-[1rem] w-full"
+      >
         <p>dsdfsdfdgfd</p>
         <p>dsdfsdfdgfd</p>
         <p>dsdfsdfdgfd</p>
