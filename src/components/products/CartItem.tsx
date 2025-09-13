3a29bb84803f88372c4894e66788df893d4121cc
Mitra Roshanfekr <mitraroshanfekr05@gmail.com>
2025-09-14 02:02:52 +0330

    cart completed

--- src/components/products/CartItem.tsx
+++ src/components/products/CartItem.tsx
@@ -1,4 +1,5 @@
-
+import { useContext } from "react";
+import { CartItemsContext } from "../../store/cartStore";
 
 type CartItemProps = {
   id: number;
@@ -9,13 +10,41 @@ type CartItemProps = {
   count: number;
 };
 
-const CartItem = (item:CartItemProps) => {
-  return (
-    <div className='h-fit'>
-        <img src={item.image} alt="dsfs" className="w-[2rem]" />
-        
-    </div>
-  )
-}
+const CartItem = (item: CartItemProps) => {
+  const { cartItems, setCartItems, removeFromCart } =
+    useContext(CartItemsContext);
 
-export default CartItem
+  function handleDelete(id: number) {
+    if (quantity > 1) {
+      const idx = cartItems.findIndex((p) => p.id === id);
+      if (idx !== -1) {
+        const updated = [...cartItems];
+        updated.splice(idx, 1);
+        setCartItems(updated);
+      }
+    } else {
+      removeFromCart(id);
+    }
+  }
+  const quantity = cartItems.filter((i) => item.id === i.id).length;
+  const totalPrice = (item.price * quantity).toFixed(2);
+
+  return (
+    <main className="w-full">
+      <div className="h-fit filter-box grid grid-cols-5 justify-between w-full cart-grid items-center">
+        <img src={item.image} alt="dsfs" className="w-[3rem]" />
+        <p>{item.title}</p>
+        <p>{quantity}</p>
+        <p>{totalPrice}</p>
+        <img
+          className="cursor-pointer"
+          onClick={() => handleDelete(item.id)}
+          src="/assets/icons/close.svg"
+          alt=""
+        />
+      </div>
+    </main>
+  );
+};
+
+export default CartItem;
--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -3,13 +3,31 @@ import CartItem from "../components/products/CartItem";
 import { CartItemsContext } from "../store/cartStore";
 
 const Cart = () => {
+  const { cartItems } = useContext(CartItemsContext);
 
-    const {cartItems, removeFromCart}=useContext(CartItemsContext)
+  const uniqueCartItems = Array.from(
+    new Map(cartItems.map((item) => [item.id, item])).values()
+  );
+
+  const totalItems = cartItems.length;
+
+  const totalPrice = cartItems.reduce((acc, cur) => {
+    acc += cur.price;
+    return acc;
+  }, 0);
+
   return (
-    <main>
-        <h1>Cart</h1>
-      <ul className="productsList p-[2rem] w-full flex flex-col gap-[2rem] md:grid md:grid-cols-3 lg:grid-cols-4 justify-center items-center align-middle md:mx-auto">
-        {cartItems?.map((product) => (
+    <main className="flex flex-col gap-[2rem] p-8">
+      <h1>Cart</h1>
+      <div className="h-fit filter-box grid grid-cols-5 justify-between w-full cart-grid">
+        <h3></h3>
+        <h3>Title</h3>
+        <h3>Quantity</h3>
+        <h3>Total Price</h3>
+        <h3></h3>
+      </div>
+      <ul className="productsList p-[2rem] w-full flex flex-col gap-[3rem] justify-center items-center align-middle md:mx-auto">
+        {uniqueCartItems?.map((product) => (
           <CartItem
             id={product.id}
             key={product.id}
@@ -21,6 +39,10 @@ const Cart = () => {
           />
         ))}
       </ul>
+      <div className="filter-box w-fit">
+        <h3>total Items: {totalItems}</h3>
+        <h3>total Price: {totalPrice.toFixed(2)}</h3>
+      </div>
     </main>
   );
 };
