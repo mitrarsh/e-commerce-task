c8b8e1c37bb7c912ab11705145296969f6d13c52
Mitra Roshanfekr <mitraroshanfekr05@gmail.com>
2025-09-13 20:58:27 +0330

    product details page completed

--- src/pages/productDetails.tsx
+++ src/pages/productDetails.tsx
@@ -1,8 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
-import { useParams } from "react-router-dom";
-import { fetchProductsById } from "../utils/productHttp";
-import LoadingIndicator from "../components/UI/Loadingindicator";
+import { Link, useParams } from "react-router-dom";
 import ErrorBlock from "../components/UI/ErrorBlock";
+import LoadingIndicator from "../components/UI/Loadingindicator";
+import { fetchProductsById } from "../utils/productHttp";
 
 const ProductDetails = () => {
   const { id } = useParams<{ id: string }>();
@@ -20,9 +20,33 @@ const ProductDetails = () => {
       error instanceof Error ? error.message : "Something went wrong";
     return <ErrorBlock title="Fetching Error" message={message} />;
   }
-  return(
-    <main>
-        
+  return (
+    <main className="flex flex-col justify-center w-[100%] gap-[2rem] lg:items-center margin">
+      <Link to='/' className="w-[90%]">
+        <img
+        className="w-[3rem] rotate-180 lg:arrow lg:w-[4rem] "
+        src="/assets/icons/arrow-up.svg"
+      />
+      </Link>
+      <div className="lg:flex-row lg:justify-self-center">
+        <div className="w-[90%] bg-white rounded-[1rem] flex flex-col p-8 gap-[4rem] lg:flex-row ">
+          <div className="w-[70%] mx-auto align-self-center flex justify-center lg:w-[fit] ">
+            <img src={data?.image} />
+          </div>
+          <div className="filter-box flex flex-col gap-[2rem]  h-fit lg:self-center">
+            <h2>{data?.title}</h2>
+            <p className="text-[2rem]">{data?.description}</p>
+            <p className="text-2xl">${data?.price}</p>
+            <div className="flex justify-between">
+              <div className="flex flex-row">
+                <p>{data?.rating.rate}</p>
+                <img src="/assets/icons/Star-medium.svg" alt="" />
+              </div>
+              <p>{data?.rating.count} reviews</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
