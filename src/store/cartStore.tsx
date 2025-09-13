842b771af564d13c91f7165237176e2d7a04e8f5
Mitra Roshanfekr <mitraroshanfekr05@gmail.com>
2025-09-13 22:13:14 +0330

    cart logic provided

--- src/store/cartStore.tsx
+++ src/store/cartStore.tsx
@@ -1,17 +1,31 @@
-import { createContext, useState, type ReactNode } from "react";
+import { createContext, useEffect, useState, type ReactNode } from "react";
+import type { Product } from "../utils/productHttp";
 
 type CartItemsContextType = {
     cartItems:any[];
     setCartItems: React.Dispatch<React.SetStateAction<any[]>>;
+    addToCart: React.Dispatch<React.SetStateAction<any[]>>;
+    removeFromCart: React.Dispatch<React.SetStateAction<any[]>>;
 }
 
-export const CartItemsContext = createContext<CartItemsContextType>({ cartItems: [], setCartItems:()=>{} });
+export const CartItemsContext = createContext<CartItemsContextType>({ cartItems: [], setCartItems:()=>{}, addToCart:()=>{}, removeFromCart:()=>{} });
 
 export function CartItemsContextProvider({children}:{ children: ReactNode }) {
   const [cartItems, setCartItems] = useState<any[]>([]);
 
+  useEffect(()=>{
+    localStorage.setItem("cart Iiems", JSON.stringify(cartItems) )
+  },[cartItems])
+
+  const addToCart= (item:Product)=>{
+    setCartItems([...cartItems,item])
+  }
+  const removeFromCart=(id:number)=>{
+    setCartItems(cartItems.filter(((item)=>item.id !==id)))
+  }
+
   return (
-    <CartItemsContext.Provider value={{ cartItems, setCartItems }}>
+    <CartItemsContext.Provider value={{ cartItems, setCartItems, addToCart, removeFromCart }}>
       {children}
     </CartItemsContext.Provider>
   );
